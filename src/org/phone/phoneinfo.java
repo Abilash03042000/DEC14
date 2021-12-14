4b243cac758d4ef033da491a345c914a5be5cd07
Abilash03042000 <abilashkumarasamy@gmail.com>
2021-12-14 14:19:59 +0530

    today git

---
+++ src/org/phone/phoneinfo.java
@@ -0,0 +1,32 @@
+package org.phone;
+
+public class phoneinfo {
+private void phonename() {
+	System.out.println("redmi");
+
+}
+private void phonemieinum() {
+	System.out.println("android");
+
+}
+private void camera() {
+	System.out.println("12 mp");
+
+}
+private void stroage() {
+	System.out.println("64 gp");
+
+}
+private void osname() {
+  System.out.println("octacore");
+
+}
+public static void main(String[] args) {
+	phoneinfo hi=new phoneinfo();
+	hi.phonename();
+	hi.phonemieinum();
+	hi.camera();
+	hi.osname();
+	hi.stroage();
+}
+}
